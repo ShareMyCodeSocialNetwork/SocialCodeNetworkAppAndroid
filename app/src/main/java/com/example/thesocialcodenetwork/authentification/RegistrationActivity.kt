a574a5cb1f128292d5cce775cb086976ca6b83df
Meziane270 <moulmas@myges.fr>
2022-03-17 01:23:45 +0100

    project initial commit

---
+++ app/src/main/java/com/example/thesocialcodenetwork/authentification/RegistrationActivity.kt
@@ -0,0 +1,4 @@
+package com.example.thesocialcodenetwork.authentification
+
+class RegistrationActivity {
+}
---
+++ app/src/main/java/com/example/thesocialcodenetwork/splash/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.example.thesocialcodenetwork.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.thesocialcodenetwork.MainActivity
+import com.example.thesocialcodenetwork.R
+
+@SuppressLint("CustomSplashScreen")
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        with(R.id.splash_animation) { }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+}
